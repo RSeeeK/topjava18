a6057cf6ffd10f9383df438ff61f2643d2fa8e1a
Arslan <Agrabadanov@gmail.com>
2019-11-30 16:24:54 +0300

    8_02_HW07_rest_controller

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -173,7 +173,7 @@ public class MealsUtil {
 //        return values.stream().flatMap(Function.identity()).collect(Collectors.toList());
 //    }
 
-    private static MealTo createTo(Meal meal, boolean excess) {
+    public static MealTo createTo(Meal meal, boolean excess) {
         return new MealTo(meal.getId(), meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess);
     }
 }
